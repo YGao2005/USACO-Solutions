c8488a018e687a701a9f0ba15bdbc48672e5ed09
SomeRandomWeirdDood <41841169+SomeRandomWeirdDood@users.noreply.github.com>
2020-06-07 18:40:56 -0700

    added teleport file

---
+++ src/main/java/teleport.java
@@ -0,0 +1,10 @@
+
+import java.io.*;
+public class teleport {
+    public static void main(String[] args) throws IOException{
+        BufferedReader f = new BufferedReader(new FileReader("teleport.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("teleport.out")));
+        String allValues = f.readLine();
+
+    }
+}

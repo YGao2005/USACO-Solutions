a956fd9ad0a09e73638daeaa4aa460f2f092733f
Jun Gao <jun@tripactions.com>
2020-06-07 18:46:27 -0700

    parse string to a list of integer

--- src/main/java/teleport.java
+++ src/main/java/teleport.java
@@ -1,10 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.io.*;
 public class teleport {
     public static void main(String[] args) throws IOException{
         BufferedReader f = new BufferedReader(new FileReader("teleport.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("teleport.out")));
         String allValues = f.readLine();
+        List<Integer> intValues = new ArrayList<>();
+        for (String value : allValues.split(" ")) {
+            intValues.add(Integer.parseInt(value));
+        }
 
     }
+
 }
